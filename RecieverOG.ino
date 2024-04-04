4994a3192d5cf45cefa71f1975e2b3c9ce0bb8b7
AAgarwal04 <121470779+AAgarwal04@users.noreply.github.com>
2024-04-03 22:54:42 -0500

    Add files via upload
    Simple Receiver

---
+++ RecieverOG.ino
@@ -0,0 +1,54 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+//Structure example to receive data
+//Must match the sender structure
+typedef struct test {
+  int data;
+} test;
+
+//Create a struct_message called myData
+test myData;
+
+//callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("x: ");
+  Serial.println(myData.data);
+  Serial.println(*mac);
+}
+
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(9600);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  OnDataRecv();
+}
